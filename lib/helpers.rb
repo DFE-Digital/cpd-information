dd07bfb0e55c7cb292d490435f827485abe6801d
James Rose <james@jbpr.net>
2022-04-22 10:51:16 +0100

    Use Prototype Kit

--- lib/helpers.rb
+++
@@ -1,5 +0,0 @@
-Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |f| require f }
-
-use_helper Nanoc::Helpers::Rendering
-use_helper Nanoc::Helpers::LinkTo
-use_helper Nanoc::Helpers::Breadcrumbs
