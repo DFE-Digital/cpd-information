43ded03768f5b839ab7064549ca3bb6a0c40f15c
Mark Avery <averycontentdesign@gmail.com>
2022-04-26 10:29:43 +0100

    Upload Sprint 7 content
    This is the version we'll be testing.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Professional development for teachers and school leaders',
 
   // Default port that prototype runs on
   port: '3000',
